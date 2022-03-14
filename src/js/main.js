5d37ec72e07b5517efaf66458048bdcdbfb3e162
Maksym <maksymRozhko7@gmail.com>
2022-03-15 01:07:14 +0200

    feat: dropdown menu component

--- src/js/main.js
+++ src/js/main.js
@@ -9,5 +9,38 @@ $('[data-count="second"]').on('click', () => {
 });
 
 $('button').eq(2).on('click', () => {
-    $('.w-500').fadeToggle(800);
+    $('.dropdown__button').fadeToggle(800);
 });
+
+$('.wrap').html(
+    `
+    <div class="dropdown">
+        <button type="button" id="dropdownMenuBtn2" class="btn btn-primary dropdown__button">
+            Dropdown button
+        </button>
+        <ul data-toggle-id="dropdownMenuBtn2" class="dropdown__menu">
+            <li class="dropdown__item">
+                <a href="#" class="dropdown__link">
+                    first link
+                </a>
+            </li>
+            <li class="dropdown__item">
+                <a href="#" class="dropdown__link">
+                    second link
+                </a>
+            </li>
+            <li class="dropdown__item">
+                <a href="#" class="dropdown__link">
+                    third link
+                </a>
+            </li>
+            <li class="dropdown__item">
+                <a href="#" class="dropdown__link">
+                    fourth link
+                </a>
+            </li>
+        </ul>
+    </div>
+    `
+);
+$('#dropdownMenuBtn2').dropdown();
