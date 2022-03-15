2f5dd7171be593924bc54cd7731262a1b7ec7bd3
Maksym <maksymRozhko7@gmail.com>
2022-03-15 02:34:12 +0200

    feat: dynamic modal; refactor: modal

--- src/js/lib/components/modal.js
+++ src/js/lib/components/modal.js
@@ -1,34 +1,93 @@
 import $ from "../core";
 
-$.prototype.modal = function() {
+$.prototype.modal = function(created) {
     for(let i = 0; i < this.length; i++) {
         const target = this[i].getAttribute('data-target');
 
         $(this[i]).click((e) => {
             e.preventDefault();
             $(target).fadeIn(500);
-
             document.body.style.overflow = 'hidden';
         });
-    };
 
-    const closeElements = document.querySelectorAll('[data-close');
+        const closeElements = document.querySelectorAll(`${target} [data-close]`);
 
-    closeElements.forEach(elem => {
-        $(elem).click(() => {
-            $('.modal').fadeOut(500);
-            document.body.style.overflow = '';
+        closeElements.forEach(elem => {
+            $(elem).click(() => {
+                $(target).fadeOut(500);
+                document.body.style.overflow = '';
+
+                if (created) {
+                    document.querySelector(target).remove();
+                }
+            });
         });
-    });
 
-    $('.modal').click((e) => {
-        const target = e.target;
+        $(target).click((e) => {
+            if (e.target.classList.contains('modal')) {
+                $(target).fadeOut(500);
+                document.body.style.overflow = '';
 
-        if (target.classList.contains('modal')) {
-            $('.modal').fadeOut(500);
-            document.body.style.overflow = '';
-        }
-    });
+                if (created) {
+                    document.querySelector(target).remove();
+                }
+            }
+        });
+    };
 };
 
 $('[data-toggle="modal"]').modal();
+
+$.prototype.createModal = function({text, btns} = {}) {
+    for (let i = 0; i < this.length; i++) {
+        let modal = document.createElement('div');
+        modal.classList.add('modal');
+        modal.setAttribute('id', this[i].getAttribute('data-target').slice(1));
+
+        // btns = {count: num, settings: [[text, classNames =[], close, callback]]}
+        const buttons = [];
+
+        for (let j = 0; j < btns.count; j++) {
+            let btn = document.createElement('button');
+            btn.classList.add('btn', ...btns.settings[j][1]);
+            btn.textContent = btns.settings[j][0];
+
+            if (btns.settings[j][2]) {
+                btn.setAttribute('data-close', 'true');
+            }
+
+            if (btns.settings[j][3] && typeof(btns.settings[j][3]) === 'function') {
+                btn.addEventListener('click', btns.settings[j][3]);
+            }
+
+            buttons.push(btn);
+        }
+
+        modal.innerHTML = `
+            <div class="modal__dialog">
+                <div class="modal__content">
+                    <button data-close class="modal__close">
+                        <span>&times;</span>
+                    </button>
+                    <div class="modal__header">
+                        <h2 class="modal__title">
+                            ${text.title}
+                        </h2>
+                    </div>
+                    <div class="modal__body">
+                        <p class="modal__text">
+                            ${text.descr}
+                        </p>
+                    </div>
+                    <div class="modal__footer"></div>
+                </div>
+            </div>
+        `;
+
+        modal.querySelector('.modal__footer').append(...buttons);
+
+        document.body.append(modal);
+        $(this[i]).modal(true);
+        $(this[i].getAttribute('data-target')).fadeIn(500);
+    };
+};
--- src/js/main.js
+++ src/js/main.js
@@ -44,3 +44,36 @@ $('.wrap').html(
     `
 );
 $('#dropdownMenuBtn2').dropdown();
+
+$('#trigger').click(() => $('#trigger').createModal({
+    text: {
+        title: 'Dynamic modal title',
+        descr: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloribus corrupti eum, et ad, alias commodi earum ipsa saepe necessitatibus rerum dolor aliquam numquam mollitia!',
+    },
+    btns: {
+        count: 3,
+        settings: [
+            [
+                'Close',
+                ['btn-danger', 'mr-10'],
+                true,
+            ],
+            [
+                'Save changes',
+                ['btn-success', 'mr-10'],
+                false,
+                function() {
+                    this.parentNode.previousElementSibling.children[0].textContent = 'Modal data saved';
+                },
+            ],
+            [
+                'Another btn',
+                ['btn-warning', 'mr-10'],
+                false,
+                function() {
+                    this.parentNode.previousElementSibling.children[0].textContent = 'Something went wrong...';
+                },
+            ]
+        ]
+    }
+}));
